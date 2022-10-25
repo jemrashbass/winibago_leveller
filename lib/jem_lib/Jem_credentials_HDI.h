bdf37f47a4d894ea829097563707fe27184f89fd
Jem Rashbass <jemrashbass@gmail.com>
2022-10-25 19:23:18 +0100

    first commit

---
+++ lib/jem_lib/Jem_credentials_HDI.h
@@ -0,0 +1,10 @@
+//This is the Jem_credentials_HDI.h file
+//place in a folder jem_lib inside lib folder for project
+//add #include <Jem_credentials_HDI.h>
+
+#define WIFI_SSID  "HealthDataInsight"
+#define WIFI_PASSWORD "qu1ckstartgu1de"
+#define MQTT_PORT 1883
+#define MQTT_ID  "jem"
+#define MQTT_PASSWORD  "Th1spassw0rd"
+#define MQTT_HOST IPAddress(10, 0, 1, 30)
